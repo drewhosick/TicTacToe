ea47957f5b3d63c6d385c6ed37e40a13a3bd6b4b
Drew Hosick <drewhosick@gmail.com>
2021-10-26 19:54:23 -0400

    added pseudo-code for add X O logic.

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const boxes = document.querySelectorAll('div');
 
+let trackXAndO = [[],[],[],[],[],[],[],[],[]];
+
 console.log(boxes);
 
-const checkForWin = () => {
-    const wins = [x11x21x31, x21x22x32, x31x32x33, x11x22x33, x31x22x31];
-    wins.forEach(win => {
-        
-    });
+// const checkForWin = () => {
+    
+    
+// };
+
+const addXO = (location, player) => {
+    if (location has no x or o in trackXandO) {
+        // then add the X or O to it
+        // check if it's player x or o that's playing
+        // then call a check on the win
+        // if no win the move onto the next turn
+    } else (illegal move) {
+        // tell users that the last move was illegal and that someone already
+        // chose that spot.
+        // do not go to next turn
+    }
+};
+
+const winCheck = () => {
+    // check for a winner;
 };
