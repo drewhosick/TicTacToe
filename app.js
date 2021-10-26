1d2a8bf4afa41cb93dbd4db21dd6d48badd56150
Drew Hosick <drewhosick@gmail.com>
2021-10-26 19:26:26 -0400

    Started working on logic for checking win

---
+++ app.js
@@ -0,0 +1,10 @@
+const boxes = document.querySelectorAll('div');
+
+console.log(boxes);
+
+const checkForWin = () => {
+    const wins = [x11x21x31, x21x22x32, x31x32x33, x11x22x33, x31x22x31];
+    wins.forEach(win => {
+        
+    });
+};
