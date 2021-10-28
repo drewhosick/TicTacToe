abd31054b9cf269b0e7100da7a79dc2baa6549bd
Drew Hosick <drewhosick@gmail.com>
2021-10-28 09:25:44 -0400

    refactored code to use array instead of object for board

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 const boxes = document.querySelectorAll('div');
 
-let trackXAndO = {x11: '', x21: '', x31: '', x12: '', x22: '', x32: '',
-    x13: '', x23: '', x33: ''}; //key value pair
+let board = ['','','','','','','','','']; // create array for board
 
-let turn = 0;
+let turn = 0; // set turn to turn 0 so we can track who's turn it is
 
 const addXO = (location, player) => {
+    
+    /* split up location id so we can check board location without
+    the b in front due to id naming conventions not allowing
+    a number at the start. */
+    let boardLoc = location.split(''); 
+    
+    /* Function nested inside the addXO function to make it easier
+    to recall original location */
     const changeBoard = (loc, xOrO) => {
         boxes.forEach(box => {
-            if (box.classList.contains(loc)) {
+            // check to see if the current itteration of box matches
+            if (box.id == location) {
+                
                 if (xOrO === 'O') {
-                    box.firstChild.classList.remove('fa-times','fas');
+                    // If it's 'O' turn, remove the 'X' class and add circle
+                    box.firstChild.classList.remove('fa-times','fas'); 
                     box.firstChild.classList.add('fa-circle', 'far');
                 }
+                // make the letter appear
                 box.firstChild.classList.remove('green-yellow');
                 box.firstChild.classList.add('black');
             }
         })
     };
 
-    // after figuring the spot for the x or o and then add it if not
-    if (trackXAndO[location] != '') {
-        // illegal move add alert later
+    // This is where the board changes happen if it's not an illegal move
+    if (board[boardLoc[1]] != '') {
+        alert('That spot is already used');
     } else {
         if (player === 'even') {
-            trackXAndO[location] = 'X';
-            changeBoard(location, 'X');
+            board[boardLoc[1]] = 'X';
+            changeBoard(boardLoc[1], 'X');
         } else {
-            trackXAndO[location] = 'O';
-            changeBoard(location, 'O');
+            board[boardLoc[1]] = 'O';
+            changeBoard(boardLoc[1], 'O');
         }
         turn++;
     }
@@ -36,14 +47,14 @@ const addXO = (location, player) => {
 };
 
 const winCheck = () => {
-    let Win1 = trackXAndO.x11 + trackXAndO.x21 + trackXAndO.x31;
-    let Win2 = trackXAndO.x21 + trackXAndO.x22 + trackXAndO.x23;
-    let Win3 = trackXAndO.x31 + trackXAndO.x32 + trackXAndO.x33;
-    let Win4 = trackXAndO.x11 + trackXAndO.x12 + trackXAndO.x13;
-    let Win5 = trackXAndO.x12 + trackXAndO.x22 + trackXAndO.x32;
-    let Win6 = trackXAndO.x13 + trackXAndO.x23 + trackXAndO.x33;
-    let Win7 = trackXAndO.x11 + trackXAndO.x22 + trackXAndO.x33;
-    let Win8 = trackXAndO.x13 + trackXAndO.x22 + trackXAndO.x31;
+    let Win1 = board[0] + board[1] + board[2];
+    let Win2 = board[3] + board[4] + board[5];
+    let Win3 = board[6] + board[7] + board[8];
+    let Win4 = board[0] + board[3] + board[6];
+    let Win5 = board[1] + board[4] + board[7];
+    let Win6 = board[2] + board[5] + board[8];
+    let Win7 = board[0] + board[4] + board[8];
+    let Win8 = board[2] + board[4] + board[6];
 
     if (Win1 === 'XXX' || Win2 === 'XXX'  || Win3 === 'XXX' || Win4 === 'XXX' || Win5 === 'XXX' || Win6 === 'XXX' || Win7 === 'XXX' || Win8 === 'XXX') {
         alert('X is Winner');
@@ -57,13 +68,10 @@ boxes.forEach(box => {
     box.addEventListener('click', (e) => {
         // this is where we call addXO and then send the box target and which player is playing
         if (turn % 2 === 0 ) {  // Checks that number is even with no remainder
-            addXO(box.classList.value, 'even');
-
+            addXO(box.id, 'even');
+            console.log(box.id);
         } else {
-            addXO(box.classList.value, 'odd');
+            addXO(box.id, 'odd');
         }
-        
-
-
     });
 });
